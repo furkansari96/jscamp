2e795a075781f80b6377dee380a5bc6e7cc5faff
Furkan Sarı <sarifurkan1996@gmail.com>
2021-10-23 16:51:16 +0300

    *19_10_2021

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun=9.20
+let dolarBugun=9.30
+
+{
+
+}
+
+console.log(dolarDun)
+
+const euroDun=11.2
+console.log(euroDun)
+
+let konutKredileri=["Konut Kredisi","Emlak Kredisi","Taşıt Kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
